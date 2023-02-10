0c744e04a89a883182e1e19cfd5395f6d4726bb3
hamzamain <mohammadmainuddin211@gmail.com>
2023-02-10 13:24:15 +0600

    delete exe file

--- try.js
+++ try.js
@@ -1,14 +1,12 @@
 const nums = [3, 2, 4];
-const target = 5;
+const target = 7;
 
 const sum = (nums, target) => {
-  for (var i = 0; i < nums.length; i++) {
-    var item = nums[i];
-    for (var j = 0; j < nums.length; j++) {
-      var item2 = nums[j];
-      var sum = item + item2;
-
-      if (sum === target) {
+  for (let i = 0; i < nums.length; i++) {
+    const first = nums[i];
+    for (let j = 1; j < nums.length; j++) {
+      const second = nums[j];
+      if (first + second === target) {
         return [i, j];
       }
     }
